3e8c5dbe0e0c3427a2cda604bb9ab4e2aaefaf76
chtoeii <77672561+chtoeii@users.noreply.github.com>
2021-06-23 09:04:54 +0700

    Create App.js

---
+++ Edit2/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavbarComp from './Component/NavbarComp';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComp/>
+    </div>
+  );
+}
+
+export default App;
