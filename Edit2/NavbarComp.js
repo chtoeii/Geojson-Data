69c7a5e842b424a110e169f77d4dfa488b38b5b3
punnbaza <85600706+punnbaza@users.noreply.github.com>
2021-06-23 09:33:09 +0700

    Update NavbarComp.js

--- Edit2/NavbarComp.js
+++ Edit2/NavbarComp.js
@@ -34,29 +34,33 @@ class NavbarComp extends Component {
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
 
-                                <NavDropdown title="Month" id="basic-nav-dropdown">
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Primary', 'June2020', e)}> June 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Success', 'July2020', e)}>July 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Danger', 'August2020', e)}>August 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Info', 'September2020', e)}>September 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Light', 'October2020', e)}>October 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'November2020', e)}>November 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'December2020', e)}>December 2020</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'January2021', e)}>January 2021</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'February2021', e)}>February 2021</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'March2021', e)}>March 2021</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'April2021', e)}>April 2021</NavDropdown.Item>
-                                    <NavDropdown.Divider />
+                                <NavDropdown title="Years" id="basic-nav-dropdown">
+                                    <NavDropdown title="2020" id="basic-nav-dropdown">
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Primary', 'June2020', e)}> June 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Success', 'July2020', e)}>July 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Danger', 'August2020', e)}>August 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Info', 'September2020', e)}>September 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Light', 'October2020', e)}>October 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'November2020', e)}>November 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'December2020', e)}>December 2020</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                    </NavDropdown>
+                                    <NavDropdown title="2021" id="basic-nav-dropdown">    
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'January2021', e)}>January 2021</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'February2021', e)}>February 2021</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'March2021', e)}>March 2021</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={e => this.changeBtn('Dark', 'April2021', e)}>April 2021</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                    </NavDropdown>
                                     <NavDropdown.Item onClick={this.resetTheme}>Pipe</NavDropdown.Item>
                                 </NavDropdown>
 
